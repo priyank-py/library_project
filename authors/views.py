487a278b128ccfa966481042a128e508aa371dbc
Admin <Admin>
2019-11-13 18:30:00 +0530

    managing templates

--- authors/views.py
+++ authors/views.py
@@ -11,5 +11,10 @@ def details(request):
 
     books_for_each_author = [Book.objects.all().filter(author=i) for i in all_authors]
     print(len(books_for_each_author[0]))
+
+    context = {
+        'all_authors': all_authors,
+    }
     
-    return render(request, 'author/details.html')
+    return render(request, 'author/details.html', context)
+
