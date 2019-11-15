1ee21b56f2259adab05275f1688d5978605d1aeb
priyank <ipriyank007@hotmail.com>
2019-11-15 18:41:53 +0530

    book listings added

--- books/views.py
+++ books/views.py
@@ -7,3 +7,10 @@ def home(request):
     all_books = Book.objects.all()
     print(all_books)
     return render(request, 'main/index.html', {'all_books': all_books})
+
+def listings(request):
+    all_books = Book.objects.all()
+    context = {
+        'all_books': all_books,
+    }
+    return render(request, 'books/listings.html', context)
