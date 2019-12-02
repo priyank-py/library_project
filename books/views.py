b814567f32851ac0c3d2d25a4a5fe0bb2960185b
priyank <ipriyank007@hotmail.com>
2019-12-02 18:25:23 +0530

    requirement.txt added, filter for listings

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404 
 from .models import Book
+from django.db.models import Q
 
 # Create your views here.
 
@@ -9,7 +10,14 @@ def home(request):
     return render(request, 'main/index.html', {'all_books': all_books})
 
 def listings(request):
-    all_books = Book.objects.all()
+    query = request.GET.get('q')
+    if not query == None:
+        all_books = Book.objects.all().filter(
+            Q(name__icontains=query) | 
+            Q(author__name__icontains=query)
+        )
+    else:
+        all_books = Book.objects.all()
     
     
     context = {
